8457712af8a49c31aeee8fadabeccab87049e13f
rksadocloudgit <56008512+rksadocloudgit@users.noreply.github.com>
2024-07-13 16:07:50 +0530

    Upd2

--- hello.py
+++ hello.py
@@ -5,7 +5,7 @@ app = Flask(__name__)
 
 @app.route('/')
 def hello_world():
-    return 'Hello World! - RK update 1'
+    return 'Hello World! - RK update -1'
 
 if __name__ == '__main__':
     app.run()
